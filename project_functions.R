6b5c9f9f06477e02eabb8f066a41829e749898f7
Adam Campbell <campbead@gmail.com>
2021-06-19 11:16:20 +1200

    added generate total offer wins table

--- project_functions.R
+++ project_functions.R
@@ -8,3 +8,34 @@ add_chosen_offer <- function (player_data) {
       ChosenOffer == HigherOffer ~ "Higher" ))
   return(output_data)
 }
+
+
+# generate total offer wins table
+get_total_offer_wins <- function(players){
+  Total_offers <- players %>%
+    add_chosen_offer () %>%
+    mutate(win = ifelse(HTH_Result >0, TRUE, FALSE)) %>%
+    group_by(OfferTaken, win) %>%
+    summarise(
+      total_count = n()
+    ) %>%
+    ungroup() %>%
+    group_by(OfferTaken) %>%
+    summarise(
+      total_total = sum(total_count)
+    )
+
+  Total_offers_wins <- players %>%
+    add_chosen_offer () %>%
+    mutate(win = ifelse(HTH_Result >0, TRUE, FALSE)) %>%
+    group_by(OfferTaken, win) %>%
+    summarise(
+      total_count = n()
+    )
+
+  Total_offers_wins <- left_join(Total_offers_wins, Total_offers, by = "OfferTaken") %>%
+    filter(win == TRUE) %>%
+    mutate(pct = total_count/total_total)
+
+  return(Total_offers_wins)
+}

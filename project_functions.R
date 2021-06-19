fd93d64ba543900e936cfe3adda3f9aac4c27c8f
Adam Campbell <campbead@gmail.com>
2021-06-19 14:27:42 +1200

    add calc_win_pct_by_chosen_offer_taken_offer_binned

--- project_functions.R
+++ project_functions.R
@@ -9,7 +9,6 @@ add_chosen_offer <- function (player_data) {
   return(output_data)
 }
 
-
 # generate total offer wins table
 get_total_offer_wins <- function(players){
   Total_offers <- players %>%
@@ -35,7 +34,46 @@ get_total_offer_wins <- function(players){
 
   Total_offers_wins <- left_join(Total_offers_wins, Total_offers, by = "OfferTaken") %>%
     filter(win == TRUE) %>%
-    mutate(pct = total_count/total_total)
+    mutate(pct = total_count/total_total) %>%
+    mutate(
+      OfferTaken = factor(OfferTaken,
+                          levels = c("Higher", "Middle", "Lower")
+      )
+    )
 
   return(Total_offers_wins)
+}
+
+calc_win_pct_by_chosen_offer_taken_offer_binned <- function(data, bin_size){
+  # modify player data for win information
+  temp_data <- data %>%
+    add_chosen_offer() %>%
+    # add column for win
+    mutate(win = ifelse(HTH_Result >0, TRUE, FALSE))
+
+  # compute total times each chosen offer and offer taken pair is selected
+  total_data <- temp_data %>%
+    # round chosen offer to nearest 1000
+    mutate(ChosenOffer = round(ChosenOffer / bin_size)* bin_size) %>%
+    group_by(ChosenOffer, OfferTaken) %>%
+    # count all times a pair of chosen offer and offer take is selected
+    summarise(total_time_selected= n())
+
+  # count
+  ind_data <- temp_data %>%
+    mutate(ChosenOffer = round(ChosenOffer / bin_size)* bin_size) %>%
+    group_by(ChosenOffer, OfferTaken, win) %>%
+    summarise(wins = n())
+
+  plot_data <- left_join(ind_data,total_data,
+                         by = c("ChosenOffer" = "ChosenOffer",
+                                "OfferTaken" = "OfferTaken" )) %>%
+    filter (win == TRUE) %>%
+    mutate(win_pct = wins/total_time_selected) %>%
+    mutate(OfferTaken = factor(
+      OfferTaken,
+      levels = c("Higher", "Middle", "Lower")
+    )) %>%
+    select(-win)
+  return(plot_data)
 }

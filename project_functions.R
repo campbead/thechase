efcc2a541a44b5d42a1f392312620e750952fbd2
Adam Campbell <campbead@gmail.com>
2021-07-13 20:07:42 +1200

    linting

--- project_functions.R
+++ project_functions.R
@@ -1,6 +1,6 @@
 
 # adds a column to the player data of the chosen offer.
-add_chosen_offer <- function (player_data) {
+add_chosen_offer <- function(player_data) {
   output_data <- player_data %>%
     mutate(OfferTaken = case_when(
       ChosenOffer == LowerOffer ~ "Lower",
@@ -12,8 +12,8 @@ add_chosen_offer <- function (player_data) {
 # generate total offer wins table
 get_total_offer_wins <- function(players){
   Total_offers <- players %>%
-    add_chosen_offer () %>%
-    mutate(win = ifelse(HTH_Result >0, TRUE, FALSE)) %>%
+    add_chosen_offer() %>%
+    mutate(win = ifelse(HTH_Result > 0, TRUE, FALSE)) %>%
     group_by(OfferTaken, win) %>%
     summarise(
       total_count = n()
@@ -25,8 +25,8 @@ get_total_offer_wins <- function(players){
     )
 
   Total_offers_wins <- players %>%
-    add_chosen_offer () %>%
-    mutate(win = ifelse(HTH_Result >0, TRUE, FALSE)) %>%
+    add_chosen_offer() %>%
+    mutate(win = ifelse(HTH_Result > 0, TRUE, FALSE)) %>%
     group_by(OfferTaken, win) %>%
     summarise(
       total_count = n()
@@ -49,26 +49,26 @@ calc_win_pct_by_chosen_offer_taken_offer_binned <- function(data, bin_size){
   temp_data <- data %>%
     add_chosen_offer() %>%
     # add column for win
-    mutate(win = ifelse(HTH_Result >0, TRUE, FALSE))
+    mutate(win = ifelse(HTH_Result > 0, TRUE, FALSE))
 
   # compute total times each chosen offer and offer taken pair is selected
   total_data <- temp_data %>%
     # round chosen offer to nearest 1000
-    mutate(ChosenOffer = round(ChosenOffer / bin_size)* bin_size) %>%
+    mutate(ChosenOffer = round(ChosenOffer / bin_size) * bin_size) %>%
     group_by(ChosenOffer, OfferTaken) %>%
     # count all times a pair of chosen offer and offer take is selected
-    summarise(total_time_selected= n())
+    summarise(total_time_selected = n())
 
   # count
   ind_data <- temp_data %>%
-    mutate(ChosenOffer = round(ChosenOffer / bin_size)* bin_size) %>%
+    mutate(ChosenOffer = round(ChosenOffer / bin_size) * bin_size) %>%
     group_by(ChosenOffer, OfferTaken, win) %>%
     summarise(wins = n())
 
   plot_data <- left_join(ind_data,total_data,
                          by = c("ChosenOffer" = "ChosenOffer",
                                 "OfferTaken" = "OfferTaken" )) %>%
-    filter (win == TRUE) %>%
+    filter(win == TRUE) %>%
     mutate(win_pct = wins/total_time_selected) %>%
     mutate(OfferTaken = factor(
       OfferTaken,
@@ -82,7 +82,7 @@ calc_win_pct_by_chosen_offer_taken_offer_binned <- function(data, bin_size){
 calc_win_pct_by_correct_answers_taken_offer <- function(data){
 
   temp_data <- data %>%
-    mutate(CorrectAnswers = CashBuilder /1000) %>%
+    mutate(CorrectAnswers = CashBuilder / 1000) %>%
     add_chosen_offer() %>%
     mutate(win = ifelse(HTH_Result > 0, TRUE, FALSE))
 
